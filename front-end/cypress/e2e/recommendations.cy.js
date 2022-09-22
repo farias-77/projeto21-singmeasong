0a9f79e9c52358f571dddd8358f34ea03939059f
farias-77 <gabrielfalves2003@gmail.com>
2022-09-22 14:59:59 -0300

    feat: upvote & downvote cypress tests

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -10,7 +10,6 @@ describe("Testa criação de recomendação", () => {
             name: faker.lorem.words(3),
             youtubeLink: "https://www.youtube.com/watch?v=zeJ3KpmgIwA",
         };
-
         cy.createNewRecommendation(recommendation);
 
         cy.contains(recommendation.name).should("be.visible");
@@ -27,5 +26,58 @@ describe("Testa criação de recomendação", () => {
         cy.on("window:alert", (t) => {
             expect(t).to.contains("Error creating recommendation!");
         });
+    });
+});
+
+describe("Testa upvote e downvpte", () => {
+    it("Testa upvote", () => {
+        const recommendation = {
+            name: faker.lorem.words(3),
+            youtubeLink: "https://www.youtube.com/watch?v=zeJ3KpmgIwA",
+        };
+        cy.createNewRecommendation(recommendation);
+
+        cy.contains(0).as("recommendationScore");
+
+        cy.get("#arrowUp").click();
+
+        cy.on("@recommendationScore", (t) => {
+            expect(t).to.contains(1);
+        });
+    });
+
+    it("Testa downvote", () => {
+        const recommendation = {
+            name: faker.lorem.words(3),
+            youtubeLink: "https://www.youtube.com/watch?v=zeJ3KpmgIwA",
+        };
+        cy.createNewRecommendation(recommendation);
+
+        cy.contains(0).as("recommendationScore");
+
+        cy.get("#arrowDown").click();
+
+        cy.on("@recommendationScore", (t) => {
+            expect(t).to.contains("-1");
+        });
+    });
+
+    it("Testa downvote com delete", () => {
+        const recommendation = {
+            name: faker.lorem.words(3),
+            youtubeLink: "https://www.youtube.com/watch?v=zeJ3KpmgIwA",
+        };
+        cy.createNewRecommendation(recommendation);
+
+        cy.get("#arrowDown").click();
+        cy.get("#arrowDown").click();
+        cy.get("#arrowDown").click();
+        cy.get("#arrowDown").click();
+        cy.get("#arrowDown").click();
+        cy.get("#arrowDown").click();
+
+        cy.contains("No recommendations yet! Create your own :)").should(
+            "be.visible"
+        );
     });
 });
